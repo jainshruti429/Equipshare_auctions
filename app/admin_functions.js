eb6c22c8c14914ee8dcbd804269064d02fe1f58e
piyush agrawal <piyushagrawal521@gmail.com>
2018-07-14 12:51:22 +0530

    changes

--- app/admin_functions.js
+++ app/admin_functions.js
@@ -1116,12 +1116,19 @@ module.exports = {
     show_equipment_requests2: function(req,res){
         res.render("./user_requested.ejs", {new_equip: req.new_equip, used_equip: req.used_equip, proposals:req.proposals, username:req.session.name});
     },
-    find_fields : function(req,res){
+    find_fields : function(req,res,fields){
     	fields=[];
     	connection.query("SELECT * FROM equipment_master WHERE subcategory=?",[req.query.subcategory],function(err,rows){
     		if(err) throw err;
     		else{
     			if(rows.length>0){
+    				for(var i=2; i<(fields.length-4);i++)
+    				{
+    					if(rows[0][fields[i].name]){
+    						fields.push(rows[0][fields[i].name);
+    				         console.log(fields);
+    				 	}
+    				}
     				
                 
     			}
